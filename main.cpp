e777aa8435526dc5d6a20be31f3916496fb19a96
Yusuf <yusufrehmanit@gmail.com>
2026-06-19 12:13:50 -0700

    Complete Lab 0

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// Muhammad Rehman
+// Lab #0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    int sum;
+
+    cout << "\nMy name is Muhammad Yusuf Rehman" << endl;
+
+    cout << "\nEnter the first number > ";
+    cin >> num1;
+
+    cout << "\nEnter the second number > ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "\nThe sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
+
+/*
+My name is Muhammad Yusuf Rehman
+
+Enter the first number > 5
+
+Enter the second number > 3
+
+The sum of 5 and 3 is 8
+*/
